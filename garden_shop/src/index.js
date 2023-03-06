3f96e840c84f6ebc05b335940faa8e0e6b282670
Evgeny <bril424@mail.ru>
2023-03-06 18:48:55 +0100

    HashRouter added

--- garden_shop/src/index.js
+++ garden_shop/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter} from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './store'
 
@@ -9,9 +9,9 @@ import { store } from './store'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Provider store={store}>
-        <BrowserRouter>
+        <HashRouter>
             <App />
-        </BrowserRouter>
+        </HashRouter>
     </Provider>
 );
 
